d6c525afffda7da88bf27e332996b8ecd7cc58a0
iigmir <roc120j@gmail.com>
2017-09-15 00:54:26 +0800

    Make controller, init views, add admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.new({
+    :email => 'root@127.0.0.1',
+    :password => 'abc"123',
+    :password_confirmation => 'abc"123'
+}).save

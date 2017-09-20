b479c0d774d516b8bdb61f4d465ea9ff4687ad3f
iigmir <roc120j@gmail.com>
2017-09-20 18:19:05 +0800

    Add issue helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    def issue_status_text( input_num )
+        case input_num
+        when 0
+            t('issues.status_number.unconfirmed')
+        when 1
+            t('issues.status_number.confirmed')
+        when 2
+            t('issues.status_number.fixed')
+        when 3
+            t('issues.status_number.wontfix')
+        when 4
+            t('issues.status_number.noreproduce')
+        when "list"
+            "1: #{t('issues.status_number.noreproduce')}"
+        else
+            t('issues.status_number.other')
+        end
+    end
 end

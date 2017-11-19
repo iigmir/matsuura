772f302459a549a090aa0dcd5e57f65b66333448
iigmir <roc120j@gmail.com>
2017-11-20 01:22:10 +0800

    Re-resources :issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :issues
   devise_for :users
   resources :home
+  resources :issues
+  resources :projects
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
